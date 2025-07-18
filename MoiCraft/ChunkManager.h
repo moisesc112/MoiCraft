9e7ff15280763ac06be07707d5de561e70c5c0e7
Moises Castro <moisescastro112@gmail.com>
2025-07-18 12:44:30 -0700

    Fixed face rendering between chunks

--- MoiCraft/ChunkManager.h
+++ MoiCraft/ChunkManager.h
@@ -10,32 +10,36 @@
 #include "Block.h"
 
 #define CHUNK_WIDTH 16
-#define CHUNK_HEIGHT 5
+#define CHUNK_HEIGHT 2
 #define CHUNK_DEPTH 16
+
+class WorldManager;
 
 class ChunkManager
 {
 public:
 	ChunkManager();
 	ChunkManager(glm::ivec3 position);
+	void initializeMesh();
 	void generateMesh();
 	void draw(Shader& shader);
 	unsigned int getVAO();
 	unsigned int getVBO();
 	void deleteVAO();
 	void deleteVBO();
-
+	bool isBlockAir(const glm::ivec3& localPos);
+	void setWorld(WorldManager* worldManager);
+	void addFace(const std::vector<float>& faceData, const glm::ivec3& position);
+	bool isFaceVisible(const glm::ivec3& position, const glm::ivec3& direction);
 private:
 	Block block;
 	Block blocks[CHUNK_WIDTH][CHUNK_HEIGHT][CHUNK_DEPTH];
 	unsigned int VAO;
 	unsigned int VBO;
-	glm::vec3 chunkPosition;
+	glm::ivec3 chunkPosition;
 	std::vector<float> vertices;
 	std::vector<std::vector<float>> faceData;
-
-	void addFace(const std::vector<float>& faceData, const glm::ivec3& position);
-	bool isFaceVisible(const glm::ivec3& position, const glm::ivec3& direction);
+	WorldManager* world;
 };
 
 #endif 
--- MoiCraft/WorldManager.cpp
+++ MoiCraft/WorldManager.cpp
@@ -2,18 +2,43 @@
 
 WorldManager::WorldManager()
 {
-	loadChunk(0, 0);
-	loadChunk(1, 0);
-	loadChunk(1, 1);
+
+
+	//loadChunk(0, 0, false);
+	//loadChunk(1, 0, false);
+	//loadChunk(1, 1, false);
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			loadChunk(i, j, false);
+		}
+	}
+
+	for(auto it = chunks.begin(); it != chunks.end(); ++it) {
+		ChunkManager* chunk = it->second;
+		chunk->setWorld(this);
+	}
+
+	for (auto it = chunks.begin(); it != chunks.end(); ++it) {
+		ChunkManager* chunk = it->second;
+		chunk->initializeMesh();
+	}
 }
 
 
-void WorldManager::loadChunk(float x, float z)
+void WorldManager::loadChunk(float x, float z, bool initialize)
 {
 	ChunkCoord chunkCoord(x, z);
 	if (chunks.find(chunkCoord) == chunks.end())
 	{
-		chunks[chunkCoord] = new ChunkManager(glm::vec3(x * CHUNK_WIDTH, 0.0f, z * CHUNK_DEPTH));
+		chunks[chunkCoord] = new ChunkManager(glm::ivec3(x * CHUNK_WIDTH, 0.0f, z * CHUNK_DEPTH));
+		if (initialize)
+		{
+			chunks[chunkCoord]->setWorld(this);
+			chunks[chunkCoord]->initializeMesh();
+		}
 	}
 }
 
@@ -23,10 +48,49 @@ void WorldManager::draw(Shader& ourShader)
 	{
 		ChunkManager* chunk = pair.second;
 		chunk->draw(ourShader);
+
 	}
 }
 
 void WorldManager::update(const Camera& camera)
 {
 
+}
+
+bool WorldManager::isBlockAir(const glm::ivec3& worldPos)
+{
+	int chunkX = worldPos.x / CHUNK_WIDTH;
+	int chunkZ = worldPos.z / CHUNK_DEPTH;
+
+	if (worldPos.x < 0)
+	{
+		chunkX--;
+	}
+	if (worldPos.z < 0)
+	{
+		chunkZ--;
+	}
+
+	ChunkCoord chunkCoord(chunkX, chunkZ);
+	auto it = chunks.find(chunkCoord);
+	if (it == chunks.end())
+	{
+		return true;
+	}
+
+	ChunkManager* chunk = it->second;
+
+	glm::ivec3 localPos = {
+		worldPos.x - chunkX * CHUNK_WIDTH,
+		worldPos.y,
+		worldPos.z - chunkZ * CHUNK_DEPTH
+	};
+
+
+	if (localPos.y < 0 || localPos.y >= CHUNK_HEIGHT)
+	{
+		return true;
+	}
+
+	return chunk->isBlockAir(localPos);
 }
