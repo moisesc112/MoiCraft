b05be7d602914ffca5a429db287052188740b14f
Moises Castro <mcastro60@gatech.edu>
2025-07-20 22:52:28 -0700

    Added temporary solution to render 3 textures of grass block

--- MoiCraft/Includes/shader.fs
+++ MoiCraft/Includes/shader.fs
@@ -2,13 +2,25 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
+flat in int faceID;
+
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
+uniform sampler2D texture_GrassTop;
+uniform sampler2D texture_GrassSide;
+uniform sampler2D texture_Dirt;
 void main()
 {
-    //FragColor = texture(texture1, TexCoord);
+
+    if (faceID == 0)
+        FragColor = texture(texture_GrassTop, TexCoord);
+    else if (faceID == 1)
+        FragColor = texture(texture_Dirt, TexCoord);
+    else
+        FragColor = texture(texture_GrassSide, TexCoord);
+
     //FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
-    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    //FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.0);
 }
--- MoiCraft/Includes/shader.vs
+++ MoiCraft/Includes/shader.vs
@@ -1,8 +1,10 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
+layout (location = 2) in float aFaceID;
 
 out vec2 TexCoord;
+flat out int faceID;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -13,4 +15,5 @@ void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
 	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
+	faceID = int(aFaceID);
 }
